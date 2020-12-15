c40b5cf9c214415b87bc1701f158ad21e587c7a4
Dima <dchaika1990@gmail.com>
2020-12-15 10:30:40 +0200

    add styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 import './lib/lib'
 
-$('button').on('click', function () {
-    $('div').eq(2).toggleClass('active');
-});
-
-$('div').click(function (e) {
-    e.stopPropagation()
-    console.log($(this).index());
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
 })
-
-$('button').fadeIn(1800);
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
+})
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+})
