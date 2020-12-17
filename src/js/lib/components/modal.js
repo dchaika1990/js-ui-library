7765e191c372df2432e4c9e1c43650a440a9ac21
dchaika1990 <dchaika@inbox.ru>
2020-12-17 23:15:53 +0200

    modal component din

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,28 +1,73 @@
 import $ from '../core';
 
-$.prototype.modal = function () {
-	for (let i = 0; i < this.length; i++) {
-		const target = $(this[i]).attr('data-target');
-		$(this[i]).click((e) => {
-			e.preventDefault();
-			$(target).fadeIn(500);
-			document.body.style.overflow = 'hidden';
-		})
-	}
+$.prototype.modal = function (created) {
+    for (let i = 0; i < this.length; i++) {
+        const target = $(this[i]).attr('data-target');
+        $(this[i]).click((e) => {
+            e.preventDefault();
+            $(target).fadeIn(500);
+            document.body.style.overflow = 'hidden';
+        })
 
-	$('[data-close]').elem().forEach(elem => {
-		$(elem).click((e) => {
-			$('.modal').fadeOut(500);
-			document.body.style.overflow = '';
-		})
-	});
+        $(`${target} [data-close]`).elem().forEach(elem => {
+            $(elem).click((e) => {
+                _closeModal()
+            })
+        });
 
-	$('.modal').click(e => {
-		if (e.target.classList.contains('modal')) {
-			$('.modal').fadeOut(500);
-			document.body.style.overflow = '';
-		}
-	})
+        $(target).click(e => {
+            if (e.target.classList.contains('modal')) {
+                _closeModal()
+            }
+        })
+
+        function _closeModal() {
+            $(target).fadeOut(500);
+            document.body.style.overflow = '';
+            if (created) $(target)[0].remove();
+        }
+    }
 }
 
-$('[data-toggle="modal"]').modal();
+// $('[data-toggle="modal"]').modal();
+
+$.prototype.createModal = function ({text, btns} = {}) {
+    for (let i = 0; i < this.length; i++) {
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+
+        const buttons = [];
+        for (let j = 0; j < btns.count; j++) {
+            let btn = document.createElement('button');
+            const {settings} = btns;
+            btn.classList.add('btn', ...settings[j][1]);
+            btn.textContent = settings[j][0];
+            if (settings[j][2]) btn.setAttribute('data-close', 'true');
+            if (settings[j][3] && typeof (settings[j][3]) === 'function') {
+                btn.addEventListener('click', settings[j][3])
+            }
+            buttons.push(btn);
+        }
+
+        modal.innerHTML = `
+             <div class="modal-dialog">
+                <div class="modal-content">
+                    <button class="close" data-close>
+                        <span>&times;</span>
+                    </button>
+                    <div class="modal-header">
+                        <div class="modal-title">${text.title}</div>
+                    </div>
+                    <div class="modal-body">${text.body}</div>
+                    <div class="modal-footer"></div>
+                </div>
+            </div>
+		`;
+
+        modal.querySelector('.modal-footer').append(...buttons);
+        document.body.appendChild(modal);
+        $(this[i]).modal(true);
+        $(this[i].getAttribute('data-target')).fadeIn(500)
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,58 @@ $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 })
 
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Title',
+        body: 'lorem lorem lorem'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success', 'mr-10'],
+                false,
+                () => {
+                    alert('Data have saved!')
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning'],
+                false,
+                () => {
+                    alert('Data have saved!')
+                }
+            ],
+        ]
+    }
+}))
+
+$('#trigger2').click(function(){
+    $('#trigger2').createModal({
+        text: {
+            title: 'Title 2',
+            body: 'lorem lorem lorem lorem lorem lorem lorem lorem'
+        },
+        btns: {
+            count: 1,
+            settings: [
+                [
+                    'Close',
+                    ['btn-danger', 'mr-10'],
+                    true
+                ]
+            ]
+        }
+    })
+})
+
 // $('.wrap').html(
 //     `
 //         <div class="dropdown">
